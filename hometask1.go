4a3fe387de2beb7447af451a6ec9c063f65fd6d9
kitglor <kitglor@mail.ru>
2019-10-01 15:03:10 +0300

    up

--- hometask1.go
+++ hometask1.go
@@ -13,7 +13,22 @@ func main() {
 	var y float64
 	fmt.Scanf("%f", &y)
 
+	fmt.Print("Enter z: ")
+	var z string
+	fmt.Scanf("%s", &z)
+	fmt.Println(z)
 
+	switch z {
+		case "*":
+			fmt.Println(x * y)
 
-	fmt.Println(x + y)
-}
+		case "-":
+			fmt.Println(x - y)
+		case "+":
+			fmt.Println(x + y)
+		case "/":
+			fmt.Println(x / y)
+		default:
+			fmt.Println("err")
+
+		}
